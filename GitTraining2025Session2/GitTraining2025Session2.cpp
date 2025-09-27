68970898e4c4d06ccc03a15394a8e3989534896e
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-09-27 13:09:38 -0400

    Add project files.

---
+++ GitTraining2025Session2/GitTraining2025Session2.cpp
@@ -0,0 +1,9 @@
+// GitTraining2025Session2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Git Session 2!\n";
+}
